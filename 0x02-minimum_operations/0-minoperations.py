d7ca62b06e45b62d25c20f3a11aaa03b600052fe
MoedCode <mohamed71291@gmail.com>
2024-04-25 03:36:05 +0200

    Thu Apr 25 03:36:05 EET 2024

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,19 +12,18 @@ def minOperations(n):
     Return:
         result: number of operations needed, int
     """
-
-    if n <= 0:
+    if n <= 1:
         return 0
 
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-
-    for i in range(2, n + 1):
-        for j in range(1, i // 2 + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n] if dp[n] != float('inf') else 0
+    result = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            result += i
+            n /= i
+            i -= 1
+        i += 1
+    return int(result)
 
 
 # Test cases
