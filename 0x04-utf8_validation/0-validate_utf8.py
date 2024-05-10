6414c5f724bc8380a775e77efda69c9197d2a54b
MoedCode <mohamed71291@gmail.com>
2024-05-10 04:35:31 +0300

    Fri May 10 04:35:31 EEST 2024

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    Description: Checks if the given list of integers represents a valid UTF-8 encoding.
+
+    Parameters:
+    - data: list of integers
+
+    Returns:
+    - True if the data is a valid UTF-8 encoding, otherwise False
+    """
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
+
+
+if __name__ == "__main__":
+    data = [65]
+    print(validUTF8(data))
+
+    data = [80, 121, 116, 104, 111, 110, 32,
+            105, 115, 32, 99, 111, 111, 108, 33]
+    print(validUTF8(data))
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data))
---
+++ 0x04-utf8_validation/temp.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%", -6);
+}
+
+0b1110
+0b111000
