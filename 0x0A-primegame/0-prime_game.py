5f5ec1920b007b15bbf76b87fd7d5f6f442dd848
MoedCode <mohamed71291@gmail.com>
2024-06-20 21:30:54 +0300

    Thu Jun 20 21:30:54 EEST 2024

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,27 @@
 #!/usr/bin/python3
-
-def sieve_of_eratosthenes(max_n):
-    """Generate a list of prime numbers up to max_n using the Sieve of Eratosthenes."""
-    is_prime = [True] * (max_n + 1)
-    is_prime[0] = is_prime[1] = False
-    p = 2
-    while (p * p <= max_n):
-        if is_prime[p]:
-            for i in range(p * p, max_n + 1, p):
-                is_prime[i] = False
-        p += 1
-    return is_prime
+"""
+Maria and Ben are playing a game.
+Given a set of consecutive integers starting from 1 up to and including n,
+they take turns choosing a prime number from the set and removing that number
+and its multiples from the set. The player that cannot make a move loses the game.
+"""
 
 
 def isWinner(x, nums):
-    """Determine the winner of the prime game."""
-    if not nums or x <= 0:
+    """ Determine who the winner of the game is """
+    if not nums or x < 1:
         return None
-
-    max_n = max(nums)
-    is_prime = sieve_of_eratosthenes(max_n)
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        primes = [i for i in range(2, n + 1) if is_prime[i]]
-        moves = 0
-        while primes:
-            prime = primes[0]
-            moves += 1
-            primes = [p for p in primes if p % prime != 0]
-
-        if moves % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+    n = max(nums)
+    primes = [True for _ in range(max(n + 1, 2))]
+    primes[0:2] = [False, False]
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+    primes = [i for i in range(n + 1) if primes[i]]
+    m = 0
+    for i in nums:
+        m += sum(prime <= i for prime in primes)
+    if m % 2 == 0:
         return "Ben"
-    else:
-        return None
-
-
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+    return "Maria"
