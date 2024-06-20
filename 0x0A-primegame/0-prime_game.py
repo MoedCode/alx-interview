de887cd7217013de9bb7b221f3b1ad790bdd2ab5
MoedCode <mohamed71291@gmail.com>
2024-06-20 21:30:12 +0300

    Thu Jun 20 21:30:12 EEST 2024

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
-def is_prime(n: int) -> bool:
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
 def sieve_of_eratosthenes(max_n):
     """Generate a list of prime numbers up to max_n using the Sieve of Eratosthenes."""
     is_prime = [True] * (max_n + 1)
@@ -20,17 +11,6 @@ def sieve_of_eratosthenes(max_n):
                 is_prime[i] = False
         p += 1
     return is_prime
-
-
-def simulate_game(n, is_prime):
-    """Simulate the game for a single round with given n."""
-    primes = [i for i in range(2, n + 1) if is_prime[i]]
-    moves = 0
-    while primes:
-        prime = primes[0]
-        moves += 1
-        primes = [p for p in primes if p % prime != 0]
-    return moves
 
 
 def isWinner(x, nums):
@@ -45,7 +25,13 @@ def isWinner(x, nums):
     ben_wins = 0
 
     for n in nums:
-        moves = simulate_game(n, is_prime)
+        primes = [i for i in range(2, n + 1) if is_prime[i]]
+        moves = 0
+        while primes:
+            prime = primes[0]
+            moves += 1
+            primes = [p for p in primes if p % prime != 0]
+
         if moves % 2 == 1:
             maria_wins += 1
         else:
@@ -60,5 +46,4 @@ def isWinner(x, nums):
 
 
 if __name__ == "__main__":
-    # Example usage
     print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
